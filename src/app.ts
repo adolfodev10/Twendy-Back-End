3d82ff303bf082c1cfe7a0b38cddf7c1dcd0e426
Adolfo Monteiro Manuel <adolfomanueldev10@gmail.com>
2025-12-15 20:59:40 +0000

    fix: add dotenv and remove any folder or file and update others

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
+import dotenv from "dotenv";
+
+import authRoutes from "./modules/auth/auth.routes";
+import usuarioRoutes from "./modules/usuarios/usuario.routes";
+import servicoRoutes from "./modules/servicos/servico.routes";
+
+dotenv.config();
+
+const app = Fastify({ logger: true });
+
+app.register(cors);
+app.register(jwt, { secret: process.env.JWT_SECRET! });
+app.register(multipart);
+
+app.register(swagger);
+app.register(swaggerUI, { routePrefix: "/docs" });
+
+app.register(authRoutes, { prefix: "/auth" });
+app.register(usuarioRoutes, { prefix: "/usuarios" });
+app.register(servicoRoutes, { prefix: "/servicos" });
+
+export default app;
